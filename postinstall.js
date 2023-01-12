e832e9cc86ee19fdcba301528ad425dcdd4755d3
Ardo Al-Jaf <a.aljaf@gmail.com>
2023-01-12 17:32:55 +0000

    init

---
+++ postinstall.js
@@ -0,0 +1,13 @@
+// part of tinyMCE React installation
+
+const fse = require("fs-extra");
+const path = require("path");
+const topDir = __dirname;
+fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
+fse.copySync(
+  path.join(topDir, "node_modules", "tinymce"),
+  path.join(topDir, "public", "tinymce"),
+  { overwrite: true }
+);
+
+// don't forget to add /public/tinymce/ to gitIgnore.
